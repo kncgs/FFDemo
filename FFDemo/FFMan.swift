43adcb0c397e59efba593bc5a50a425c37347edd
houdongbing <dongbinghouy@gmail.com>
2021-04-17 11:03:09 +0800

    init sdk

---
+++ FFDemo/FFMan.swift
@@ -0,0 +1,15 @@
+//
+//  FFMan.swift
+//  FFDemo
+//
+//  Created by DBH on 2021/4/17.
+//
+
+import Foundation
+
+public class FFMan {
+    
+    public func name() -> String {
+        "the FFman"
+    }
+}
